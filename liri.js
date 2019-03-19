5fe00819b0f63f9557dd82ee3d678be9cd71fc8c
Andrew <xie.andrew2235@gmail.com>
2019-03-19 15:06:51 -0700

    added API keys for spotify, links for OMDB and BIT, continuing to work on logic in liri.js

--- liri.js
+++ liri.js
@@ -1,10 +1,28 @@
 //
-
 require("dotenv").config();
+var axios = require("axios");
 // Spotify API
 var spotify = new Spotify(keys.spotify);
 // OMDB API
+axios.get("http://www.omdbapi.com/?t=remember+the+titans&y=&plot=short&apikey=trilogy")
+.then(
+  function(response) {
+    console.log("The movie's rating is: " , response);
+  }
+);
 // Bands In Town API
+function searchBandsInTown(artist) {
+
+    // Querying the bandsintown api for the selected artist, the ?app_id parameter is required, but can equal anything
+    var queryURL = "https://rest.bandsintown.com/artists/" + artist + "?app_id=codingbootcamp";
+    $.ajax({
+      url: queryURL,
+      method: "GET"
+    }).then(function(response) {
+      console.log(queryURL);
+      // Printing the entire object to console
+      console.log(response);
+      searchBandsInTown(inputArtist);
 // Authenticate from keys.js
 var keys = require("keys.js");
 
@@ -24,7 +42,7 @@ else {
     getSpotify("default");
 
 // else if for OMDB?
-} else if (cmd === "movie-this") {
+} if (cmd === "movie-this") {
     if(option1) {
         var nodeArgs = process.argv.slice(2).join('');
         movieName = option1;
@@ -38,4 +56,4 @@ else {
     readFile();
 } else {
     console.log("Your instructions could not be understood.")
-}
+}})}
